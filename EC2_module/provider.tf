fd49f570b79d832c8e99e505d1602ad262da50de
Pushpavathi valasa <valasapushpa13@gmail.com>
2026-04-14 10:04:15 +0530

    Update provider.tf

--- EC2_module/provider.tf
+++ EC2_module/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws" # tells the terraform to download provider from hashicorp
-      version = "5.84.0" # use this version, so it won't break and ensures smooth deployments
+      version = "6.0" # use this version, so it won't break and ensures smooth deployments
     }
   }
 }
