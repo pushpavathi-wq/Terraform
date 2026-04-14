3f7b08a83b5b2078f5d20d9756ad5ba6ddb7a595
pushpavathi-wq <valasapushpa13@gmail.com>
2026-04-14 15:41:10 +0530

    create s3 bucket

--- EC2_module/main.tf
+++ EC2_module/main.tf
@@ -1,13 +1,13 @@
 
-# AWS Instance
-resource "aws_instance" "newinstance" {
-  ami                    = "ami-0ec10929233384c7f" # Replace with a valid AMI ID
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [var.sg_id]
+# # AWS Instance
+# resource "aws_instance" "newinstance" {
+#   ami                    = "ami-0ec10929233384c7f" # Replace with a valid AMI ID
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [var.sg_id]
   
 
-  tags = {
-    Name = "TERRAFORM"
-  }
-}
+#   tags = {
+#     Name = "TERRAFORM"
+#   }
+# }
 
