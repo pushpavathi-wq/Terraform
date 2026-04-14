e3d099ee6d995e0044572f6cda76ac42194e7042
pushpavathi-wq <valasapushpa13@gmail.com>
2026-04-14 15:46:45 +0530

    create s3 bucket

--- EC2_module/backend.tf
+++ EC2_module/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "pushpavathi-terraform-state"
     key            = "ec2/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
 
   }
 }
