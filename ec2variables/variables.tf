9fb591e73204cfda6cc1e6b8342f1dc549111d68
pushpavathi-wq <valasapushpa13@gmail.com>
2026-05-20 06:43:11 +0530

    commiting dev infra

---
+++ ec2variables/variables.tf
@@ -0,0 +1,30 @@
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type for the web server"
+  default     = "t2.micro"
+}
+
+
+
+variable "ami" {
+    type = string
+    description = "creating ami for the instance"
+    default = "ami-0220d79f3f480ecf5"
+  
+}
+
+
+variable "from_port" {
+    type = number
+    description = "using variable for ingress port usage"
+    default = 22
+  
+}
+
+
+variable "to_port" {
+    type = number
+    description = "using variable for ingress port usage"
+    default = 22
+  
+}
