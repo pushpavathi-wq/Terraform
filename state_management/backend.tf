bf48a16a4325a6d4edc58ab7154d8d4f2e02f808
pushpavathi-wq <valasapushpa13@gmail.com>
2026-05-21 06:52:31 +0530

    state

--- state_management/backend.tf
+++ state_management/backend.tf
@@ -1,6 +1,7 @@
-#terraform {
- # backend "s3" {     # we can write this backend config in providers file also
-   # bucket         = "my-tf-daws82s-bucket"
-   # key            = "terraform.tfstate"
- # }
-#}
+terraform {
+  backend "s3" {     # we can write this backend config in providers file also
+    bucket         = "my-tf-daws82s-bucket"
+    key            = "terraform.tfstate"
+    dynamodb_table = "tf_state_table"
+  }
+}
