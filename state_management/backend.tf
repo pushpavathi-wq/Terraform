1971c5001757e57c288d2f4e83ad5fce4616c800
pushpavathi-wq <valasapushpa13@gmail.com>
2026-05-21 07:20:48 +0530

    commit state

--- state_management/backend.tf
+++ state_management/backend.tf
@@ -1,7 +1,11 @@
 terraform {
   backend "s3" {     # we can write this backend config in providers file also
     bucket         = "my-tf-daws82s-bucket"
-    key            = "terraform.tfstate"
-    dynamodb_table = "tf_state_table"
+    key            = "terraform.tfstate"   # unique,path to the bucket
+    region = "us-east-1"
+    dynamodb_table = "tf_state_table" # view the  state lock in tables-explore more items
   }
 }
+
+
+# for now the table and s3 bucket created manually through UI
