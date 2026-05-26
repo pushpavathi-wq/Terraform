79c109fcb2a7453fc4d6444ee78d01f35712f1a7
pushpavathi-wq <valasapushpa13@gmail.com>
2026-05-26 17:24:27 +0530

    multiple

---
+++ multiple_varss/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws={
+      version = "6.43.0"
+      source = "hashicorp/aws"
+        
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+#
---
+++ multiple_varss/variables.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+  
+}
+
+#
